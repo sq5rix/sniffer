377dd101567ec8959f42b8a11a2d991af12a1bed
sq5rix <sq5rix@gmail.com>
2019-07-19 19:41:18 +0200

    ready

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-// use std::env;
-use std::net::IpAddr;
-// use std::str::FromStr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
+use std::sync::mpsc::*;
+use std::thread;
+
 #[macro_use]
 extern crate clap;
 use clap::{App, Arg};
+
+// const MAX: u16 = 6555;
+const MAX: u16 = 65535;
 
 fn main() {
     let matches = App::new("sniffer")
@@ -12,12 +17,18 @@ fn main() {
         .after_help("Use with care...")
         .version("v0.1")
         .arg(
-            Arg::with_name("threads")
+            Arg::with_name("verbose")
+                .required(false)
+                .help("verbose ports")
+                .short("v"),
+        )
+        .arg(
+            Arg::with_name("jobs")
                 .default_value("4")
                 .number_of_values(1)
                 .takes_value(true)
-                .help("number of threads")
-                .short("t"),
+                .help("number of threads/jobs")
+                .short("j"),
         )
         .arg(
             Arg::with_name("ipaddr")
@@ -27,9 +38,60 @@ fn main() {
         )
         .get_matches();
 
-    let ipaddr = value_t!(matches, "ipaddr", IpAddr).expect("Provide IPv4 or IPv6 address ");
-    let threads = value_t!(matches, "threads", u16).expect("Provide number of threads");
+    let addr = value_t!(matches, "ipaddr", IpAddr).expect("Provide IPv4 or IPv6 address ");
+    let num_threads = value_t!(matches, "jobs", u16).expect("Provide number of threads");
+    let is_verbose = matches.is_present("verbose");
+    ();
 
-    println!("{:?}", ipaddr);
-    println!("{:?}", threads);
+    println!("Number of jobs: {}", num_threads);
+    println!("IP address: {}", addr);
+
+    let (tx, rx) = channel();
+
+    for i in 0..num_threads {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads, is_verbose);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+    println!("");
+    if is_verbose {
+        println!("Open ports:")
+    }
+    out.sort();
+    for v in out {
+        if is_verbose {
+            println!("{} is open", v);
+        } else {
+            println!("{}", v);
+        }
+    }
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16, is_verbose: bool) {
+    let mut port = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {
+                if is_verbose {
+                    println!("{}", port);
+                }
+            }
+        }
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
 }
