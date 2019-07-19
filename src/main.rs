0899e44fc66c3e5eb852978cadaa2677daaf62af
sq5rix <sq5rix@gmail.com>
2019-07-19 03:08:22 +0200

    parser quite ready

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // use std::env;
-// use std::net::IpAddr;
+use std::net::IpAddr;
 // use std::str::FromStr;
+#[macro_use]
 extern crate clap;
 use clap::{App, Arg};
 
@@ -29,8 +30,16 @@ fn main() {
                 .help("number of threads")
                 .short("t"),
         )
-        .arg(Arg::with_name("URL").required(true).help("URL to sniff"))
+        .arg(
+            Arg::with_name("ipaddr")
+                // .multiple(true)
+                .required(true)
+                .help("IP addr to sniff"),
+        )
         .get_matches();
+    let ipaddr = value_t!(matches, "ipaddr", IpAddr).expect("Provide IPv4 or IPv6 address ");
+    let threads = value_t!(matches, "threads", u16).expect("Provide number of threads");
 
-    println!("{:?}", matches);
+    println!("{:?}", ipaddr);
+    println!("{:?}", threads);
 }
